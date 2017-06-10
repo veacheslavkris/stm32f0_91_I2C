d98490284ed175eb52e2e7413a5e66f361a94734
veacheslav <veacheslavkris@gmail.com>
2017-06-10 04:21:25 +0300

    i2c_transfer_receive

--- user/a_i2c.c
+++ user/a_i2c.c
@@ -8,6 +8,14 @@
   * @param  None
   * @retval None
   */
+  
+  #define TIMEOUT_AUTOEND_STOP	10
+  #define TIMEOUT_RXNE			10
+  #define TIMEOUT_NACK			10
+  #define TIMEOUT_NACK_STOP		10
+
+  #define TIMEOUT_I2C			10
+  
 	
 	
 void I2C_ConfigMstrTimingPe(I2C_TypeDef* pI2C)
@@ -131,155 +139,101 @@ void I2C_TransferConfig(I2C_TypeDef* pI2C,  uint16_t DevAddress, uint8_t Size, u
   pI2C->CR2 = tmpreg;
 }
 
-
-
-// void HAL_I2C_Master_Receive(I2C_TypeDef* pI2C, uint16_t DevAddress, uint8_t *pData, uint16_t Size, uint32_t Timeout)
-void HAL_I2C_Master_Receive(I2CStructHandle* pHI2C)
+static I2CStateEnum I2C_IsAcknowledgeFailed(I2C_TypeDef* pI2C)
 {
+	uint32_t timout = 0;
+
+	if((pI2C->ISR & I2C_ISR_NACKF) == I2C_ISR_NACKF)
+	{
+		/* Wait until STOP Flag is reset */
+		/* AutoEnd should be initiate after AF */
+		while((pI2C->ISR & I2C_ISR_STOPF) == 0U)
+		{
+			/* Check for the Timeout */
+			if(timout < TIMEOUT_NACK_STOP) timout++;
+			else return I2C_STATE_TIMEOUT_NACK_STOP;
+		}
+
+		/* Clear NACKF Flag */
+		pI2C->ICR|=I2C_ICR_NACKCF;
+
+		/* Clear STOP Flag */
+		pI2C->ICR|=I2C_ICR_STOPCF;
+		
+		/* Flush TX register */
+//		I2C_Flush_TXDR(hi2c);
+
+		/* Clear Configuration Register 2 */
+		pI2C->CR2 &= (uint32_t)~((uint32_t)(I2C_CR2_SADD | I2C_CR2_HEAD10R | I2C_CR2_NBYTES | I2C_CR2_RELOAD | I2C_CR2_RD_WRN));
+
+
+		return I2C_STATE_NACK;
+	}
+	else return I2C_STATE_NOT_NACK;
+}
+
+
+I2CStateEnum HAL_I2C_Master_Receive(I2CStructHandle* pHI2C)
+{
+	uint32_t timeout = 0;
+
 	/* Prepare transfer parameters */
 	pHI2C->RxBuff.ix_ary = 0;
-	
-	
+
 	/* Send Slave Address */
 	/* Set NBYTES to write and reload if hi2c->XferCount > MAX_NBYTE_SIZE and generate RESTART */
 	I2C_TransferConfig(pHI2C->pI2C, pHI2C->devAddress, pHI2C->RxBuff.transfer_size, I2C_AUTOEND_MODE, I2C_GENERATE_START_READ);
 
+	/* Wait until RXNE flag is set */
+	while((pHI2C->pI2C->ISR & I2C_ISR_RXNE) == 0U)
+	{
+		/* Check only one first time if a NACK is detected */
+		if(I2C_IsAcknowledgeFailed(pHI2C->pI2C) == I2C_STATE_NACK)
+		{
+			return I2C_STATE_NACK;
+		}
+
+		/* Check for the Timeout */
+		if(timeout < TIMEOUT_RXNE) timeout++;
+		else return I2C_STATE_TIMEOUT_RXNE; 
+	}
+
+	
 	while(pHI2C->RxBuff.transfer_size > 0U)
 	{
+		timeout = 0;
+		
 		/* Wait until RXNE flag is set */
-		if(!I2C_WaitOnRXNEFlagUntilTimeout)
+		while((pHI2C->pI2C->ISR & I2C_ISR_RXNE) == 0U)
 		{
-			return HAL_ERROR / return HAL_TIMEOUT;;
+			/* Check for the Timeout */
+			if(timeout < TIMEOUT_RXNE) timeout++;
+			else return I2C_STATE_TIMEOUT_RXNE; 
 		}
 
 		/* Read data from RXDR */
 		pHI2C->RxBuff.ary_data[pHI2C->RxBuff.ix_ary++] = pHI2C->pI2C->RXDR;
 		pHI2C->RxBuff.transfer_size--;
-
 	}
 
+	timeout = 0;
+	
 	/* No need to Check TC flag, with AUTOEND mode the stop is automatically generated */
 	/* Wait until STOPF flag is set */
-//    if(I2C_WaitOnSTOPFlagUntilTimeout)return HAL_ERROR / return HAL_TIMEOUT;
-	while((pHI2C->pI2C->ISR & I2C_ISR_STOPF) == 0U) continue;
-    
+	while((pHI2C->pI2C->ISR & I2C_ISR_STOPF) == 0U) 
+	{
+		if(timeout < TIMEOUT_AUTOEND_STOP)timeout++;
+		else return I2C_STATE_TIMEOUT_AUTOEND_STOP;
+	}
+
 	/* Clear STOP Flag */
 	pHI2C->pI2C->ICR|=I2C_ICR_STOPCF;
-	
+
 	/* Clear Configuration Register 2 */
 	pHI2C->pI2C->CR2 &= (uint32_t)~((uint32_t)(I2C_CR2_SADD | I2C_CR2_HEAD10R | I2C_CR2_NBYTES | I2C_CR2_RELOAD | I2C_CR2_RD_WRN));
 
-	
-	return HAL_OK;
-
+	return I2C_STATE_RX_DONE;
 }
-
-
-
-//static HAL_StatusTypeDef I2C_IsAcknowledgeFailed(I2C_TypeDef* pI2C, uint32_t Timeout, uint32_t Tickstart)
-//{
-////  if(__HAL_I2C_GET_FLAG(hi2c, I2C_FLAG_AF) == SET)
-//  if((pI2C->ISR & I2C_ISR_NACKF) == I2C_ISR_NACKF)
-//  {
-//    /* Wait until STOP Flag is reset */
-//    /* AutoEnd should be initiate after AF */
-////    while(__HAL_I2C_GET_FLAG(hi2c, I2C_FLAG_STOPF) == RESET)
-//    while((pI2C->ISR & I2C_ISR_STOPF) == 0)
-//    {
-//      /* Check for the Timeout */
-//      if(Timeout != HAL_MAX_DELAY)
-//      {
-//      if((Timeout == 0U)||((HAL_GetTick() - Tickstart) > Timeout))
-//        {
-//          hi2c->State= HAL_I2C_STATE_READY;
-//          hi2c->Mode = HAL_I2C_MODE_NONE;
-
-//          /* Process Unlocked */
-//          __HAL_UNLOCK(hi2c);
-//          return HAL_TIMEOUT;
-//        }
-//      }
-//    }
-
-//    /* Clear NACKF Flag */
-//    __HAL_I2C_CLEAR_FLAG(hi2c, I2C_FLAG_AF);
-
-//    /* Clear STOP Flag */
-//    __HAL_I2C_CLEAR_FLAG(hi2c, I2C_FLAG_STOPF);
-
-//    /* Flush TX register */
-//    I2C_Flush_TXDR(hi2c);
-
-//    /* Clear Configuration Register 2 */
-//    I2C_RESET_CR2(hi2c);
-
-//    hi2c->ErrorCode = HAL_I2C_ERROR_AF;
-//    hi2c->State= HAL_I2C_STATE_READY;
-//    hi2c->Mode = HAL_I2C_MODE_NONE;
-
-//    /* Process Unlocked */
-//    __HAL_UNLOCK(hi2c);
-
-//    return HAL_ERROR;
-//  }
-//  return HAL_OK;
-//}
-
-//static HAL_StatusTypeDef I2C_WaitOnRXNEFlagUntilTimeout(I2C_HandleTypeDef *hi2c, uint32_t Timeout, uint32_t Tickstart)
-//{
-//  while(__HAL_I2C_GET_FLAG(hi2c, I2C_FLAG_RXNE) == RESET)
-//  {
-//    /* Check if a NACK is detected */
-//    if(I2C_IsAcknowledgeFailed(hi2c, Timeout, Tickstart) != HAL_OK)
-//    {
-//      return HAL_ERROR;
-//    }
-
-//    /* Check if a STOPF is detected */
-//    if(__HAL_I2C_GET_FLAG(hi2c, I2C_FLAG_STOPF) == SET)
-//    {
-//      /* Clear STOP Flag */
-//      __HAL_I2C_CLEAR_FLAG(hi2c, I2C_FLAG_STOPF);
-
-//      /* Clear Configuration Register 2 */
-//      I2C_RESET_CR2(hi2c);
-
-//      hi2c->ErrorCode = HAL_I2C_ERROR_NONE;
-//      hi2c->State= HAL_I2C_STATE_READY;
-//      hi2c->Mode = HAL_I2C_MODE_NONE;
-
-//      /* Process Unlocked */
-//      __HAL_UNLOCK(hi2c);
-
-//      return HAL_ERROR;
-//    }
-
-//    /* Check for the Timeout */
-//    if((Timeout == 0U)||((HAL_GetTick() - Tickstart) > Timeout))
-//    {
-//      hi2c->ErrorCode |= HAL_I2C_ERROR_TIMEOUT;
-//      hi2c->State= HAL_I2C_STATE_READY;
-
-//      /* Process Unlocked */
-//      __HAL_UNLOCK(hi2c);
-
-//      return HAL_TIMEOUT;
-//    }
-//  }
-//  return HAL_OK;
-//}
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
